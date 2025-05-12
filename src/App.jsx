be988a0136dd2ad2869d7d967f58b3f1dd4fab41
aleksandre <faleqs33@gmail.com>
2025-05-12 22:42:18 +0400

    first changes

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import people from './data'
+import React from 'react'
+function App() {
+  const [person, setPerson] = React.useState(people);
+  return (
+    <div className='list-parent'>
+      <h2> {person.length} Birthdays Today </h2>
+      <div className='list'>
+          {person.map((person, index) => (
+                <div key={index} className='person'>
+                  <img src={person.img} alt={person.alt} />
+                  <section>
+                    <h2>{person.name}</h2>
+                    <p>age: {person.age}</p>
+                  </section>
+                </div>
+          ))}
+      </div>
+      <section className='btns'>
+        <button onClick={() => {setPerson([])}}>remove all</button>
+        <button onClick={() => {setPerson([...person, {name: "aleksandre", age: 20, img: "img2", alt: "aleksandres img"}])}}>add person</button>
+      </section>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,9 @@
+import pimg from "./images/ubraloses.jpg"
+const people = [
+  { name: "aleksandre", age: 20, img: pimg, alt: "person image" },
+  { name: "nino", age: 25, img: pimg, alt: "person image" },
+  { name: "martini", age: 26, img: pimg, alt: "person image" },
+  { name: "mari", age: 24, img: pimg, alt: "person image" },
+  { name: "oto", age: 25, img: pimg, alt: "person image" },
+];
+export default people;
