916f6e87fb2ac506de5a432e1ace4bc8aed51136
aleksandre <faleqs33@gmail.com>
2025-05-14 20:28:18 +0400

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import people from './data'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import ubraloses from "./images/ubraloses.jpg"
 function App() {
   const [person, setPerson] = React.useState(people);
   return (
+    
     <div className='list-parent'>
       <h2> {person.length} Birthdays Today </h2>
       <div className='list'>
@@ -19,7 +21,7 @@ function App() {
       </div>
       <section className='btns'>
         <button onClick={() => {setPerson([])}}>remove all</button>
-        <button onClick={() => {setPerson([...person, {name: "aleksandre", age: 20, img: "img2", alt: "aleksandres img"}])}}>add person</button>
+        <button onClick={() => {setPerson([...person, {name: "aleksandre", age: 20, img: ubraloses, alt: "aleksandres img"}])}}>add person</button>
       </section>
     </div>
     
